c47cde5373b08fdde48f4c934407d1a04db5f1f6
Kim <xzsa18@naver.com>
2022-10-11 16:02:17 +0900

    로그인 화면 이벤트 실습(수정 버전)

--- app/src/main/java/com/example/loginlogic/MainActivity.kt
+++ app/src/main/java/com/example/loginlogic/MainActivity.kt
@@ -18,17 +18,21 @@ class MainActivity : AppCompatActivity() {
             val inputID = UserId.text.toString()
             val inputPw = Password.text.toString()
 
-            if(inputID != "146218") {
+            if(inputID != "xzsa18@naver.com" && inputPw != "zxas146218" ) {
+                Toast.makeText(this, "아이디와 비밀번호 모두 틀렸습니다!", Toast.LENGTH_SHORT).show()
+            }
+
+            else if(inputID != "xzsa18@naver.com") {
                 Toast.makeText(this, "아이디가 일치하지 않습니다", Toast.LENGTH_SHORT).show()
             }
 
-            else if(inputPw != "146218") {
+            else if(inputPw != "zxas146218") {
                 Toast.makeText(this, "비밀번호가 틀렸습니다!", Toast.LENGTH_SHORT).show()
             }
 
             else {
                 Log.d("로그인 확인","로그인이 확인되었음")
-                Toast.makeText(this, "로그인이 완료되었습니다!", Toast.LENGTH_SHORT).show()
+                Toast.makeText(this, "로그인에 성공했습니다", Toast.LENGTH_SHORT).show()
             }
         }
     }
