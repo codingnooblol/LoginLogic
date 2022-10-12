4eb71d4c8cc08078be4d72380dc67cd56ae23c6a
Kim <xzsa18@naver.com>
2022-10-12 19:07:36 +0900

    코드에 적은 관리자 이름도 변수에 나오게 -String 가공하기

--- app/src/main/java/com/example/loginlogic/MainActivity.kt
+++ app/src/main/java/com/example/loginlogic/MainActivity.kt
@@ -17,13 +17,14 @@ class MainActivity : AppCompatActivity() {
 
             val inputID = UserId.text.toString()
             val inputPw = Password.text.toString()
+            val name = "Kim"
 
             if(inputID != "xzsa18@naver.com" && inputPw != "zxas146218" ) {
                 Toast.makeText(this, "아이디와 비밀번호 모두 틀렸습니다!", Toast.LENGTH_SHORT).show()
             }
 
             else if(inputID != "xzsa18@naver.com") {
-                Toast.makeText(this, "아이디가 일치하지 않습니다", Toast.LENGTH_SHORT).show()
+                Toast.makeText(this, "아이디가 일치하지 않습니다.", Toast.LENGTH_SHORT).show()
             }
 
             else if(inputPw != "zxas146218") {
@@ -32,7 +33,7 @@ class MainActivity : AppCompatActivity() {
 
             else {
                 Log.d("로그인 확인","로그인이 확인되었음")
-                Toast.makeText(this, "로그인에 성공했습니다", Toast.LENGTH_SHORT).show()
+                Toast.makeText(this, "${name}님이 로그인에 성공했습니다", Toast.LENGTH_SHORT).show()
             }
         }
     }
